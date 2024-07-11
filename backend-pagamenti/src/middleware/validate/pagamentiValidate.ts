ce70dc6d117d6d4aa87e83a9e74e9bf9059721e3
Arianna <s1115431@studenti.univpm.it>
2024-07-11 10:49:57 +0200

    aggiunto validator pagamenti

--- backend-pagamenti/src/middleware/validate/pagamentiValidate.ts
+++ backend-pagamenti/src/middleware/validate/pagamentiValidate.ts
@@ -1,13 +1,13 @@
 import { body, param } from 'express-validator';
 import validateRequest from './validateRequestMiddleware';
 
-export const ricaricaCreditoValidation = [
+export const ricaricaTokenValidation = [
   body('id').isInt().withMessage('ID must be an integer'),
   body('importo').isFloat({ gt: 0 }).withMessage('Importo must be positive'),
   validateRequest
 ];
 
-export const verificaCreditoValidation = [
+export const verificaTokenValidation = [
   param('id').isInt().withMessage('ID must be an integer'),
   validateRequest
 ];
--- backend-pagamenti/src/routes/pagamentiRoutes.ts
+++ backend-pagamenti/src/routes/pagamentiRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 import { payMulta, rechargeTokens, checkToken } from '../controllers/pagamentiController';
+import { ricaricaTokenValidation,
+         verificaTokenValidation,
+         pagaMultaValidation
+ } from '../middleware/validate/pagamentiValidate';
 import { authMiddleware, authorize } from '../middleware/authMiddleware';
 
 const router = Router();
@@ -7,8 +11,8 @@ console.log('----------- entro 2 backend -----------');
 
 router.use(authMiddleware);
 
-router.post('/pagamulta', authorize(['automobilista']), payMulta);
-router.get('/tokenresidui', authorize(['automobilista']), checkToken);
-router.post('/ricaricatoken', authorize(['admin']), rechargeTokens);
+router.post('/pagamulta', authorize(['automobilista']), pagaMultaValidation, payMulta);
+router.get('/tokenresidui', authorize(['automobilista']), verificaTokenValidation, checkToken);
+router.post('/ricaricatoken', authorize(['admin']), ricaricaTokenValidation, rechargeTokens);
 
 export default router;
